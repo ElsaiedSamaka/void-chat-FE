35d15942984f5106250f50d419215c0c7b0994bd
Saied <elsaied.samaka1997@gmail.com>
2023-07-15 00:20:58 +0300

    commit 11

--- src/app/pages/index/views/views.module.ts
+++ src/app/pages/index/views/views.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ChatComponent } from './chat/chat.component';
 import { GroupListComponent } from './group-list/group-list.component';
 import { PrivateListComponent } from './private-list/private-list.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, FormsModule],
   declarations: [GroupListComponent, PrivateListComponent, ChatComponent],
   exports: [GroupListComponent, PrivateListComponent, ChatComponent],
 })
