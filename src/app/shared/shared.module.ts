52aeeae5ca745155a692c944160aae810196268d
Saied <elsaied.samaka1997@gmail.com>
2023-07-15 00:16:48 +0300

    commit 11

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ModalComponent } from './components/modal/modal.component';
+import { ToastComponent } from './components/toast/toast.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  declarations: [],
-  exports: [],
+  declarations: [ModalComponent, ToastComponent],
+  exports: [ModalComponent, ToastComponent],
 })
 export class SharedModule {}
