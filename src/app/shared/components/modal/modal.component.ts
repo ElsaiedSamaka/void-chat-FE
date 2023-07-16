667af7e247214b39df244c57d2cd8f97812749ce
Saied <elsaied.samaka1997@gmail.com>
2023-07-16 08:44:35 +0300

    commit 11

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-modal',
@@ -13,16 +14,22 @@ import {
 })
 export class ModalComponent implements OnInit {
   @Output() dismiss = new EventEmitter<boolean>();
-
-  constructor(private el: ElementRef) {}
+  theme: string = '';
+  constructor(private el: ElementRef, private themeService: ThemeService) {}
 
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
+    this.getCurrentTheme();
   }
   ngOnDestory() {
     this.el.nativeElement.remove();
   }
   onDismissClick() {
     this.dismiss.emit(false);
+  }
+  getCurrentTheme() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.theme = theme;
+    });
   }
 }
