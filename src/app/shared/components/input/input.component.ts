e43394a708db3a9551a50bb41b70cc369b9ca089
Saied <elsaied.samaka1997@gmail.com>
2023-07-23 18:23:52 +0300

    commit 11

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit {
+  @Input() label: string = '';
+  @Input() control!: FormControl;
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() value: string = '';
+  @Input() required: boolean = false;
+  @Input() minlength: number = 0;
+  @Input() maxlength: number = 50;
+  @Input() pattern: string = '';
+  @Input() disabled: boolean = false;
+  @Input() readonly: boolean = false;
+  @Input() autocomplete: string = 'on';
+  @Input() autofocus: boolean = false;
+  @Input() tabindex: number = 0;
+  @Input() size: number = 50;
+  @Input() title: string = '';
+  
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { InputComponent } from './components/input/input.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { ToastComponent } from './components/toast/toast.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  declarations: [ModalComponent, ToastComponent],
-  exports: [ModalComponent, ToastComponent],
+  declarations: [ModalComponent, ToastComponent, InputComponent],
+  exports: [ModalComponent, ToastComponent, InputComponent],
 })
 export class SharedModule {}
