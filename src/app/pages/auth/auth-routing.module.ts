189741b1fedf43505c66e178f16b18c41a8bf87f
Saied <elsaied.samaka1997@gmail.com>
2023-07-24 23:55:14 +0300

    commit 11

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -8,12 +8,12 @@ const routes: Routes = [
   { path: 'signout', pathMatch: 'full', component: SignoutComponent },
   { path: 'signup', pathMatch: 'full', component: SignupComponent },
   { path: '', pathMatch: 'full', component: SigninComponent },
-  {
-    path: '**',
-    pathMatch: 'full',
-    loadChildren: () =>
-      import('../notfound/notfound.module').then((m) => m.NotfoundModule),
-  },
+  // {
+  //   path: '**',
+  //   pathMatch: 'full',
+  //   loadChildren: () =>
+  //     import('../notfound/notfound.module').then((m) => m.NotfoundModule),
+  // },
 ];
 
 @NgModule({
