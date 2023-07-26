d3148f082d475702459dd425435d6c9f8c092bac
Saied <elsaied.samaka1997@gmail.com>
2023-07-26 12:13:15 +0300

    commit 12

--- src/core/services/chat.service.ts
+++ src/core/services/chat.service.ts
@@ -1,39 +1,30 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, filter, tap } from 'rxjs';
-import { Socket, io } from 'socket.io-client';
 import { SharedService } from 'src/app/pages/index/services/shared.service';
-import { environment } from 'src/environments/environment.prod';
-import { getCookie } from '../helper/getCookie';
-import { AuthService } from './auth.service';
+import { SocketService } from './socket.service';
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  socket: Socket;
-  currentUser;
   selectedContact;
   messages$ = new BehaviorSubject<any[]>([]);
 
   constructor(
-    private authService: AuthService,
+    private socketService: SocketService,
     private sharedService: SharedService
   ) {
-    const token = getCookie('token');
-    this.socket = io(environment.api_url, {
-      query: { token, userId: this.authService.USER$.value.id },
-    });
     this.sharedService.selectedContact$.subscribe((selectedContact) => {
       this.selectedContact = selectedContact;
     });
   }
   getMessages(senderId: number, recipientId: number) {
     try {
-      this.socket.emit('getMessages', {
+      this.socketService.socket.emit('getMessages', {
         sender: senderId,
         recipient: recipientId,
       });
 
-      this.socket.on('messages', (messages) => {
+      this.socketService.socket.on('messages', (messages) => {
         this.messages$.next(messages);
       });
     } catch (error) {
@@ -42,12 +33,12 @@ export class ChatService {
   }
 
   sendMessage(senderId: number, recipientIds: number[], text: string) {
-    this.socket.emit('sendMessage', {
+    this.socketService.socket.emit('sendMessage', {
       sender: senderId,
       recipients: recipientIds,
       message: text,
     });
-    this.socket.on('newMessage', (newMessage) => {
+    this.socketService.socket.on('newMessage', (newMessage) => {
       console.log('newMessage', newMessage);
       this.messages$
         .pipe(
@@ -59,26 +50,16 @@ export class ChatService {
   }
 
   joinRoom(userId, recipientId) {
-    this.socket.emit('join', {
+    this.socketService.socket.emit('join', {
       userId: userId,
       recipientId: recipientId,
     });
   }
 
   leaveRoom(userId, recipientId) {
-    this.socket.emit('leave', {
+    this.socketService.socket.emit('leave', {
       userId: userId,
       recipientId: recipientId,
-    });
-  }
-
-  getContactedUsers(userId) {
-    console.log('called');
-    this.socket.emit('getContacts', {
-      userId: userId,
-    });
-    this.socket.on('contacts', (contacts) => {
-      console.log('contacts', contacts);
     });
   }
 }
